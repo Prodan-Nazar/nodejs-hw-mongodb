a9236f3ed87c88e741258eca023406268d1c156c
Prodan-Nazar <142262396+Prodan-Nazar@users.noreply.github.com>
2024-07-23 17:21:38 +0300

    hw6-email-and-images

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,10 @@
+export const errorHandler = (err, req, res, next) => {
+  const { status = 500, message = 'Something went wrong' } = err;
+
+  res.status(status).json({
+    status,
+    message,
+  });
+
+  next();
+};
---
+++ src/routers/contacts.js
@@ -0,0 +1,47 @@
+import { Router } from 'express';
+import { upload } from '../middlewares/multer.js';
+
+import { authenticate } from '../middlewares/authenticate.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+
+import {
+  getContactsController,
+  getContactByIdController,
+  createContactController,
+  patchContactController,
+  deleteContactController,
+} from '../controllers/contacts.js';
+
+const router = Router();
+
+router.use(authenticate);
+
+router.get('/', ctrlWrapper(getContactsController));
+
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+
+router.post(
+  '/',
+  upload.single('photo'),
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
+
+router.patch(
+  '/:contactId',
+  isValidId,
+  upload.single('photo'),
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
+
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
+
+export default router;
